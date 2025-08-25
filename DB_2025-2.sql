ef9b799879c26f052f20f624dc3ad9c57f661e99
tomines-jaeyoung <tominesss12@gmail.com>
2025-08-25 15:50:16 +0900

    Add files via upload
    1일차 DB ALTER TABLE

---
+++ DB_2025-2.sql
@@ -0,0 +1,2 @@
+create user c##dpem IDENTIFIED by 1234;
+grant dba, CONNECT, RESOURCE to c##dpem
---
+++ DB_2025-2~1.sql
@@ -0,0 +1,7 @@
+alter table 고객
+                add 가입날짜 date
+                
+alter table 고객
+                drop column 가입날짜;
+                
+        
